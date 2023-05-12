f846a74105e6ae8727c73ec7b8e85047609af257
hyeincha <runprogrmm@gmail.com>
2023-05-12 21:33:16 +0900

    feat: 헤더 left section 구현

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className='flex justify-between px-14 py-4 shadow-lg items-center'>
+      <Link to='/'>
+        <section className='flex gap-2 items-center'>
+          <img src='../public/codestates.png' alt='코드스테이츠 로고' width='42' />
+          <h1 className='text-2xl font-bold'>COZ Shopping</h1>
+        </section>
+      </Link>
+      <RxHamburgerMenu size={26} />
+    </header>
+  );
 }
 
 export default Header;
