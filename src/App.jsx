0dadf9ff643c6dc19e3e633d218042cbf68db443
hyeincha <runprogrmm@gmail.com>
2023-05-12 21:31:40 +0900

    feat: 푸터 구현, 레이아웃 구현을 위한 라우팅 수정

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
