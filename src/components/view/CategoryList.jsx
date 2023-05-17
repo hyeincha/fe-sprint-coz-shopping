2b262710085d5935d3cd637ee13c23abd81bd97e
hyeincha <runprogrmm@gmail.com>
2023-05-17 18:00:07 +0900

    refactor: 상품 타입과 카테고리 목록을 객체 형태의 상수로 관리

--- src/components/view/CategoryList.jsx
+++ src/components/view/CategoryList.jsx
@@ -1,37 +1,10 @@
 import Category from '../ui/Category.jsx';
-
-const categoryList = [
-  {
-    parameter: 'all',
-    name: '전체',
-    imgSrc: '/all.png',
-  },
-  {
-    parameter: 'product',
-    name: '상품',
-    imgSrc: '/product.png',
-  },
-  {
-    parameter: 'category',
-    name: '카테고리',
-    imgSrc: '/category.png',
-  },
-  {
-    parameter: 'exhibition',
-    name: '기획전',
-    imgSrc: '/exhibition.png',
-  },
-  {
-    parameter: 'brand',
-    name: '브랜드',
-    imgSrc: '/brand.png',
-  },
-];
+import { CATEGORY_LIST } from '../../helpers/constants.js';
 
 function CategoryList() {
   return (
     <ul className='flex m-5 gap-8 justify-center'>
-      {categoryList.map((category) => (
+      {CATEGORY_LIST.map((category) => (
         <Category key={category.name} {...category} />
       ))}
     </ul>
