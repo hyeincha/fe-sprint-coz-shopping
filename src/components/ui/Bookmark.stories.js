403c95720e82bdaa5aa5d0a776c26a70da9f6219
hyeincha <runprogrmm@gmail.com>
2023-05-25 13:57:48 +0900

    feat: 푸터/북마크/토스트 컴포넌트 스토리북 적용

---
+++ src/components/ui/Bookmark.stories.js
@@ -0,0 +1,13 @@
+import Bookmark from './Bookmark';
+
+export default {
+  title: 'Bookmark',
+  component: Bookmark,
+};
+
+export const bookmarks = {
+  args: {
+    isBookmarked: false,
+    style: '',
+  },
+};
---
+++ src/components/ui/Toast.stories.js
@@ -0,0 +1,12 @@
+import Toast from './Toast';
+
+export default {
+  title: 'Toast',
+  component: Toast,
+};
+
+export const toasts = {
+  args: {
+    isBookmarked: false,
+  },
+};
