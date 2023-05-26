5d55b9d86284f6558c30df0a1ad3ccee4a6c424a
hyeincha <runprogrmm@gmail.com>
2023-05-26 16:51:43 +0900

    refactor: 토스트 메시지 상수로 관리

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -1,9 +1,11 @@
+import { TOAST_MESSAGE } from '../../helpers/constants';
+
 function Toast({ isBookmarked }) {
   return (
     <div className='flex items-center gap-3 w-max mt-2 px-8 py-4 bg-white shadow-[0px_0px_20px_#0000002c] rounded-lg'>
       <img src={isBookmarked ? '/greystar.png' : '/yellowstar.png'} width={22} />
       <span className='font-semibold text-lg'>
-        {isBookmarked ? '상품이 북마크에서 제거되었습니다.' : '상품이 북마크에 추가되었습니다.'}
+        {isBookmarked ? TOAST_MESSAGE.REMOVE : TOAST_MESSAGE.ADD}
       </span>
     </div>
   );
